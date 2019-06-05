59a2d238d8312fd35184582b7f84f2340c962e05
joohun484 <joohun484@gmail.com>
2019-06-06 01:19:54 +0900

    Sound Added

--- OpenCV_MediaPlayer/OpenCV_MediaPlayer/main.cpp
+++ OpenCV_MediaPlayer/OpenCV_MediaPlayer/main.cpp
@@ -1,8 +1,10 @@
 #pragma once
 #include "opencv2/highgui/highgui.hpp"
 // highgui - an interface to video and image capturing.
-
+#include<SFML/Audio.hpp>
 #include <iostream>
+
+
 // The header files for performing input and output.
 
 using namespace cv;
@@ -17,6 +19,8 @@ using namespace std;
 #define KEY_A 97
 #define KEY_D 100
 #define KEY_ENTER 13
+#define KEY_W 119
+#define KEY_S 115
 
 int key;
 bool running = true;
@@ -26,6 +30,10 @@ bool rewind_sec = false;
 bool rewind_forward_sec = false;
 bool is_end = false;
 bool reset_video = false;
+sf::Sound sound;
+VideoCapture* cap;
+#define VOL_INCR_RATE 10
+#define VOL_DECR_RATE 10
 #define REWIND_TIME_RATE 1
 void printSettings() {
 	std::cout << "*************************************" << std::endl;
@@ -38,6 +46,24 @@ void printSettings() {
 	std::cout << "OTHER KEYS : Exit the Program" << std::endl;
 	std::cout << "*************************************" << std::endl;
 }
+int volume = 100;
+void incrVol() {
+	if (volume + VOL_INCR_RATE <= 100) {
+		volume += VOL_INCR_RATE;
+	}
+	else {
+		volume = 100;
+	}
+}
+void decrVol() {
+	if (volume - VOL_DECR_RATE >= 0) {
+		volume -= VOL_DECR_RATE;
+	}
+	else {
+		volume = 0;
+	}
+
+}
 void key_callback(int key) {
 	if (is_end == false) {
 		switch (key) {
@@ -46,6 +72,14 @@ void key_callback(int key) {
 		case KEY_ESC:
 			exit(1);
 		case KEY_SPACE:
+			if (running) { 
+				sound.pause(); 
+				sound.setPlayingOffset(sf::milliseconds(sound.getPlayingOffset().asMilliseconds() - cap->get(CAP_PROP_POS_MSEC)));
+			}
+			else {
+				sound.setPlayingOffset(sf::milliseconds(sound.getPlayingOffset().asMilliseconds() - cap->get(CAP_PROP_POS_MSEC)));
+				sound.play();
+			}
 			running = !running;
 			break;
 		case KEY_A:
@@ -53,6 +87,14 @@ void key_callback(int key) {
 			break;
 		case KEY_D:
 			rewind_forward_sec = true;
+			break;
+		case KEY_W:
+			incrVol();
+			sound.setVolume(volume);
+			break;
+		case KEY_S:
+			decrVol();
+			sound.setVolume(volume);
 			break;
 		default:
 			std::cout << key << std::endl;
@@ -73,15 +115,35 @@ void key_callback(int key) {
 }
 int main()
 {
-	VideoCapture cap("KakaoTalk_Video_20190513_2045_46_804.mp4");
+	int total_time; // in milliseconds
+	std::string file_loc = "KakaoTalk_Video_20190513_2045_46_804.mp4";
+	cap = new VideoCapture(file_loc);
+
+	cap->set(CAP_PROP_POS_AVI_RATIO, 1);
+	total_time = cap->get(CAP_PROP_POS_MSEC);
+	cap->set(CAP_PROP_POS_AVI_RATIO, 0);
+
+	sf::SoundBuffer buffer;
+	
+
+	// Create WAV file for audio
+	std::string wave_loc = file_loc + ".wav";
+	std::string ffmpeg_cmd = "ffmpeg.exe -i " + file_loc + " -vn " + wave_loc;
+	system(ffmpeg_cmd.c_str());
+
 	// cap is the object of class video capture that tries to capture Bumpy.mp4
-	if (!cap.isOpened())  // isOpened() returns true if capturing has been initialized.
+	if (!cap->isOpened())  // isOpened() returns true if capturing has been initialized.
 	{
 		cout << "Cannot open the video file. \n";
 		return -1;
 	}
 
-	// int fps = cap.get(CV_CAP_PROP_POS_MSEC); //get the frames per seconds of the video
+	if (!buffer.loadFromFile(wave_loc)) {
+		cout << "Cannot open wave file. \n";
+		return -1;
+	}
+	
+	// int fps = cap->get(CV_CAP_PROP_POS_MSEC); //get the frames per seconds of the video
 	// The function get is used to derive a property from the element.
 	// Example:
 	// CV_CAP_PROP_POS_MSEC :  Current Video capture timestamp.
@@ -94,45 +156,37 @@ int main()
 	// WINDOW_AUTOSIZE : The window size is automatically adjusted to fitvthe displayed image() ), and you cannot change the window size manually.
 	// WINDOW_OPENGL : The window will be created with OpenGL support.
 
-	/* iterate through first 10 frames */
-	//for (int i = 0; i < 10000; i++)
-	//{
-	//	/* set pointer to frame index i */
-	//	cap.set(cv::CAP_PROP_POS_MSEC, i);
-	//	Mat  frame;
-	//	/* capture the frame and do sth with it */
-	//	if (!cap.read(frame)) {
-	//		cout << "\n Cannot read the video file. \n";
-	//		break;
-	//	}
-	//	imshow("A_good_name", frame);
-	//	key = waitKey(0);
-	//	key_callback(key);		
-	//}
 	
+	sound.setBuffer(buffer);
+	sound.setPitch(1.1f); // audio speed
+	sound.play();
 	while (1)
 	{
+		
 		Mat frame;
 		// Mat object is a basic image container. frame is an object of Mat.
-
+		
 		if (running == true) {
+			// sync video with audio
+			sound.setPlayingOffset(sf::milliseconds(sound.getPlayingOffset().asMilliseconds() - cap->get(CAP_PROP_POS_MSEC)));
+
 			if (rewind_sec == true) {
-				int current_msec = cap.get(CAP_PROP_POS_MSEC);
-				cap.set(CAP_PROP_POS_MSEC, current_msec - 1000* REWIND_TIME_RATE);
-				//cap.set(CAP_PROP_POS_FRAMES, frame_cnt);
+				int current_msec = cap->get(CAP_PROP_POS_MSEC);
+				cap->set(CAP_PROP_POS_MSEC, current_msec - 1000* REWIND_TIME_RATE);
+				//cap->set(CAP_PROP_POS_FRAMES, frame_cnt);
 				rewind_sec = false;
 			}
 			else if (rewind_forward_sec == true) {
-				int current_msec = cap.get(CAP_PROP_POS_MSEC);
-				cap.set(CAP_PROP_POS_MSEC, current_msec + 1000 * REWIND_TIME_RATE);
+				int current_msec = cap->get(CAP_PROP_POS_MSEC);
+				cap->set(CAP_PROP_POS_MSEC, current_msec + 1000 * REWIND_TIME_RATE);
 				rewind_forward_sec = false;
 			}
 			else if (reset_video == true) {
-				cap.set(CAP_PROP_POS_FRAMES, 0);
+				cap->set(CAP_PROP_POS_FRAMES, 0);
 				reset_video = false;
 			}
 			else {
-				if (!cap.read(frame)) // if not success, break loop
+				if (!cap->read(frame)) // if not success, break loop
 					  // read() decodes and captures the next frame.
 				{
 					is_end = true;
